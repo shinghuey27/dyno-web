7f5ed4715c464a570ea57ae0882d258de5661593
shinghuey27 <shinghuey27@hotmail.com>
2021-12-16 15:41:09 +0800

    main

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,12 @@
+import './Button.css';
+import '../../index.css';
+const Button = () => {
+    return (
+      <div>
+        <button className="button">Click Here</button>
+      </div>
+    );
+  };
+  
+  export default Button;
+  
