33113eb2d99a7ab4ed8e6970b22f17d42af37943
shinghuey27 <shinghuey27@hotmail.com>
2022-01-25 09:25:38 +0800

    added comments and className styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import "./App.css";
 // import Button from "./Components/Button/Button";
 // import Label from "./Components/Label/Label";
 // import Description from "./Components/Label/Description";
-
 // import Sidebar from "./Components/Sidebar/Sidebar";
 // import Text from "./Components/Textbox/Text";
 // import Password from "./Components/Textbox/Password";
 // import Currency from "./Components/Textbox/Currency";
-
 // import Checkbox from "./Components/Checkbox/Checkbox";
 // import Switch from "./Components/Switch/Switch";
 // import Radio from "./Components/RadioButton/Radio";
@@ -18,29 +16,30 @@ import "./App.css";
 // import ErrorMessage from "./Components/Label/ErrorMessage";
 // import HelpOutline from "./Components/Icon/HelpOutline";
 // import HyperLink from "./Components/Link/HyperLink";
+// import {
+//   arrayOfStrings,
+//   options,
+//   optionsSample,
+//   topAccount,
+//   topBanks
+// } from "./SampleData/List";
+// import DropdownSearch from "./Components/Select/DropdownSearch/DropdownSearch";
+// import TextboxDyno from "./Components/Textbox/TextboxDyno";
+// import { color } from "@mui/system";
+// import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+// import { Icon } from "@mui/material";
+// import TextboxDyno_0 from "./Components/Textbox/TextboxDyno_0";
 
-import {
-  arrayOfStrings,
-  options,
-  optionsSample,
-  topAccount,
-  topBanks
-} from "./SampleData/List";
-import DropdownSearch from "./Components/Select/DropdownSearch/DropdownSearch";
 import { useState } from "react";
-import TextboxDyno from "./Components/Textbox/TextboxDyno";
-import { color } from "@mui/system";
-import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
-import { Icon } from "@mui/material";
-import TextboxDyno_0 from "./Components/Textbox/TextboxDyno_0";
 import Textbox from "./Components/Textbox/Textbox";
 
 function App() {
   const [demo, setDemo] = useState(true);
   const [inputError, setInputError] = useState(false);
-
+  // 2 methods for custom styling
   return (
     <fieldset className="field" style={{ padding: "2rem" }}>
+      {/* inline styling */}
       <Textbox
         label={{
           text: "ID Number",
@@ -49,8 +48,7 @@ function App() {
             fontSize: "14px",
             fontWeight: 400,
             fontFamily: "Lato"
-          },
-          className: "labelClass"
+          }
         }}
         tooltip={{ text: "abc" }}
         text={{
@@ -79,6 +77,24 @@ function App() {
             fontWeight: 400,
             fontFamily: "Lato"
           }
+        }}
+      />
+      {/* className styling */}
+      <Textbox
+        label={{
+          text: "ID Number 2",
+          className: "labelClass"
+        }}
+        tooltip={{ text: "abc" }}
+        text={{
+          className: "textClass"
+        }}
+        description={{
+          text: "description 2"
+        }}
+        error={{
+          text: "*Please enter valid ID number",
+          className: "errorClass"
         }}
       />
     </fieldset>
